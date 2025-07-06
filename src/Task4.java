792208997a4ceb99033c0dbce69dae69a939db6c
Azerty <nik.k.2018@mail.ru>
2025-07-06 19:23:07 +0300

    Решение задачи 4: числа от 10 до -10

---
+++ src/Task4.java
@@ -0,0 +1,8 @@
+public class Task4 {
+    public static void main(String[] args) {
+        // Вариант с циклом for
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+}
