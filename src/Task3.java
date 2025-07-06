2b79c0337c9918d2e6ff33ff98b865b624d28e87
Azerty <nik.k.2018@mail.ru>
2025-07-06 19:19:51 +0300

    Решение задачи 3: чётные числа 0-17

---
+++ src/Task3.java
@@ -0,0 +1,8 @@
+public class Task3 {
+    public static void main(String[] args) {
+        // Вариант 1: с шагом 2
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
