7f098bccef8d5feb66d4a1306e748d63fec5a676
Azerty <nik.k.2018@mail.ru>
2025-07-06 19:28:37 +0300

    Решение задачи 6: последовательность с шагом 7

---
+++ src/Task6.java
@@ -0,0 +1,8 @@
+public class Task6 {
+    public static void main(String[] args) {
+        // Вариант 1: с шагом 7
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.print(i + " ");
+        }
+    }
+}
